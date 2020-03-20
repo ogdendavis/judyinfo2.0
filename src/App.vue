7f69f78af077ee9c22cc7a52bb32d593ee42515b
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-03-20 10:38:41 -0400

    Add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero title="Welcome to JudyInfo.com!" />
+    <HelloWorld msg="Knowledge is Power" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Hero from './components/Hero.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Hero
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hero">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<style scoped>
+.hero {
+  width: 100vw;
+  height: 20rem;
+  max-height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: url('../assets/judy-on-camel.jpg') top/cover no-repeat;
+  position: relative;
+}
+.hero::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0,50,100,0.25);
+}
+h1 {
+  color: white;
+  padding-top: 5rem;
+  z-index: 1;
+}
+</style>
