4576bbf3a3e7b23b19abdef57f8ccace9e254f1c
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-03-23 10:53:50 -0400

    Add About component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,70 @@
   <div id="app">
     <Hero :title="siteTitle" />
     <HelloWorld msg="Knowledge is Power" />
+    <About
+      v-for="person in aboutPeople"
+      :key="person.id"
+      :name="person.name"
+      :hometown="person.hometown"
+      :isFemale="person.isFemale"
+      :pets="person.pets"
+    />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import Hero from './components/Hero.vue';
+import About from './components/About.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Hero
+    Hero,
+    About,
   },
   data() {
     return {
       siteTitle: 'Need info? Ask Judy!',
+      aboutPeople: [
+        {
+          id: 1,
+          name: 'Judy',
+          hometown: 'North Haven',
+          isFemale: true,
+          pets: [
+            {
+              name: 'Sidney',
+              species: 'cat',
+            },
+            {
+              name: 'Sandy',
+              species: 'husband',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Marty',
+          hometown: 'Freeport',
+          isFemale: false,
+          pets: [
+            {
+              name: 'Luna',
+              species: 'dog',
+            },
+            {
+              name: 'Hadley',
+              species: 'warrior',
+            },
+            {
+              name: 'Cora',
+              species: 'princess',
+            },
+          ],
+        },
+      ],
     };
   },
 }
