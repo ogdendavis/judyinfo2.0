0d0168c74bc30db6656875fd587c143bff722d2c
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-03-23 11:26:55 -0400

    Add Intro component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app">
+  <div id="app" v-if="loaded">
     <Hero :title="siteTitle" />
+    <Intro quote="I think, therefore I am" author="Spongebob" />
     <About
       v-for="person in aboutPeople"
       :key="person.id"
@@ -10,21 +11,31 @@
       :pets="person.pets"
     />
   </div>
+  <div class="app app--loading" v-else>
+    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+  </div>
 </template>
 
 <script>
 import Hero from './components/Hero.vue';
+import Intro from './components/Intro.vue';
 import About from './components/About.vue';
 
 export default {
   name: 'App',
   components: {
     Hero,
+    Intro,
     About,
   },
   data() {
     return {
       siteTitle: 'Need info? Ask Judy!',
+      loaded: true,
+      qotd: {
+        quote: '',
+        author: '',
+      },
       aboutPeople: [
         {
           id: 1,
@@ -78,5 +89,48 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+/* For loading spinner */
+.app--loading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #555;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #555 transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="intro">
+    <h2>Knowledge is Power!</h2>
+    <blockquote class="qotd">
+      <p class="quote">{{ quote }}</p>
+      <footer class="author">-- {{ author }}</footer>
+    </blockquote>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Intro',
+    props: {
+      quote: String,
+      author: String
+    }
+  }
+</script>
+
+<style scoped>
+  .qotd {
+    background: #444;
+    color: #fff;
+    padding: 1rem;
+  }
+  p {
+    margin-top: 0;
+  }
+</style>
