df6ffd2b88acbd1db0e0ff0c2af597814d05734f
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-04-01 09:38:52 -0400

    Remove unneeded data from App

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,24 @@
 <template>
   <div id="app">
     <Hero :title="siteTitle" />
+    <Ask />
     <router-view />
   </div>
 </template>
 
 <script>
 import Hero from './components/Hero.vue';
+import Ask from '@/components/Ask';
 
 export default {
   name: 'App',
   components: {
     Hero,
+    Ask
   },
   data() {
     return {
       siteTitle: 'Need info? Ask Judy!',
-      qotd: {
-        quote: '',
-        author: '',
-      },
-      aboutPeople: [
-        {
-          id: 1,
-          name: 'Judy',
-          hometown: 'North Haven',
-          isFemale: true,
-          pets: [
-            {
-              name: 'Sidney',
-              species: 'cat',
-            },
-            {
-              name: 'Sandy',
-              species: 'husband',
-            },
-          ],
-        },
-        {
-          id: 2,
-          name: 'Marty',
-          hometown: 'Freeport',
-          isFemale: false,
-          pets: [
-            {
-              name: 'Luna',
-              species: 'dog',
-            },
-            {
-              name: 'Hadley',
-              species: 'warrior',
-            },
-            {
-              name: 'Cora',
-              species: 'princess',
-            },
-          ],
-        },
-      ],
     };
   },
 }
